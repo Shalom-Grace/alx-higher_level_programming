2db48e9d246b27fb1de5b4ae6904dda9ad0b8287
Shalom-Grace <alolashalom@yahoo.com>
2022-08-23 08:07:04 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 
-print("Last digit of", number + "is" + number[-1])
+print("Last digit of", number, "is", [%d] .format(number[-1]))
 if number[-1] > 5:
     print("and is greater than 5")
 elif number[-1] == 0:
