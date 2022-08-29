2b8bbfa0341bbd8d88733c4ac0c4d90d361c647d
Shalom-Grace <alolashalom@yahoo.com>
2022-08-29 06:04:43 -0700

    Doing hard things

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last = ((number * -1) % 10 * -1)
-if number > 0:
+    last = ((number * -1) % 10) * -1
+else:
     last = number % 10
 if last > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last))
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i range(0, 99):
+    print("{} = {}".format(i, hex(i)))
